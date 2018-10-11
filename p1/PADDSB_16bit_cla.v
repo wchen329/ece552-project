a8d058d642ab47e36cfa1deff1d87f19634baeab
Winor Chen <wchen329@wisc.edu>
2018-10-11 01:58:44 -0500

    Added testbench for PADDSB and fixed hiZ port connection

--- p1/PADDSB_16bit_cla.v
+++ p1/PADDSB_16bit_cla.v
@@ -8,9 +8,9 @@ module PADDSB_16bit_cla(Sum, A, B);
 	input [16:0] A, B;
 
 	// Calculate word sums, saturation is handled down one level
-	addsub_4bit_cla HB_0(Sum[3:0], A[3:0], B[3:0]);
-	addsub_4bit_cla HB_1(Sum[7:4], A[7:4], B[7:4]);
-	addsub_4bit_cla HB_2(Sum[11:8], A[11:8], B[11:8]);
-	addsub_4bit_cla HB_3(Sum[15:12], A[15:12], B[15:12]);
+	addsub_4bit_cla HB_0(Sum[3:0], A[3:0], B[3:0], 0);
+	addsub_4bit_cla HB_1(Sum[7:4], A[7:4], B[7:4], 0);
+	addsub_4bit_cla HB_2(Sum[11:8], A[11:8], B[11:8], 0);
+	addsub_4bit_cla HB_3(Sum[15:12], A[15:12], B[15:12], 0);
 
 endmodule
