a19510d77878c0d3660d8da38faade978a14474a
Winor Chen <wchen329@wisc.edu>
2018-10-16 02:02:42 -0500

    Update stage.bat

--- p1/stage.bat
+++ p1/stage.bat
@@ -1,4 +1,4 @@
 @echo off
 echo This will put all project files in the same top-level folder called "p1_stage"
 pause
-FOR /R %%f IN (*.v) DO copy %%f ..\p1_all
+FOR /R %%f IN (*.v) DO copy %%f ..\p1_stage
