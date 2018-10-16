61534a27fd84357bcb1b8a6fffbdc173e8779a36
Winor Chen <wchen329@wisc.edu>
2018-10-15 22:26:11 -0500

    Changed ALU so that port connections are according to the interface. Still passes testbench.

--- p1/ALU_no_check.v
+++ p1/ALU_no_check.v
@@ -7,37 +7,37 @@
  *
  * wchen329@wisc.edu
  */
-module ALU_no_check(ALU_In1, ALU_In2, ALU_Op, ALU_Out, Flags_Out);
+module ALU_no_check(A, B, op, out, flags);
 
-	output [15:0] ALU_Out;
-	output [2:0] Flags_Out;
-	input [15:0] ALU_In1, ALU_In2;
-	input [2:0] ALU_Op; 
+	output [15:0] out;
+	output [2:0] flags;
+	input [15:0] A, B;
+	input [2:0] op; 
 
 	wire [15:0] add_out, sub_out, xor_out, red_out, sll_out, sra_out, ror_out, paddsb_out;
 	wire [2:0] add_flags; 
-	wire ALU_Out_Z, ALU_Out_V, ALU_Out_N;
+	wire out_Z, out_V, out_N;
 
 	// ALU Sub Modules
-	addsub_16bit_cla ADDSUB(add_out, ALU_Out_V, ALU_In1, ALU_In2, ALU_Op[0]);
-	xor_16bit XOR(xor_out, ALU_In1, ALU_In2); 
-	PADDSB_16bit_cla PADDSB(paddsb_out, ALU_In1, ALU_In2);
+	addsub_16bit_cla ADDSUB(add_out, out_V, A, B, op[0]);
+	xor_16bit XOR(xor_out, A, B); 
+	PADDSB_16bit_cla PADDSB(paddsb_out, A, B);
 
-	assign ALU_Out =
-	ALU_Op == 0 ? add_out :
-	ALU_Op == 1 ? add_out :
-	ALU_Op == 2 ? xor_out :
-	ALU_Op == 3 ? red_out :
-	ALU_Op == 4 ? sll_out :
-	ALU_Op == 5 ? sra_out :
-	ALU_Op == 6 ? ror_out :
-	ALU_Op == 7 ? paddsb_out : 0; 
+	assign out =
+	op == 0 ? add_out :
+	op == 1 ? add_out :
+	op == 2 ? xor_out :
+	op == 3 ? red_out :
+	op == 4 ? sll_out :
+	op == 5 ? sra_out :
+	op == 6 ? ror_out :
+	op == 7 ? paddsb_out : 0; 
 
-	assign ALU_Out_Z = ALU_Out == 0 ? 1 : 0; 
-	assign ALU_Out_N = ALU_Out[15];
+	assign out_Z = out == 0 ? 1 : 0; 
+	assign out_N = out[15];
 
-	assign Flags_Out =
-		{ALU_Out_Z, ALU_Out_V ,ALU_Out_N}; 
+	assign flags =
+		{out_Z, out_V ,out_N}; 
 
 endmodule
 
