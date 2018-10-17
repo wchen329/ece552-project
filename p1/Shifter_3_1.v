ae2dedc88f21f3c717422d4669ef1ba810e01efe
Winor Chen <wchen329@wisc.edu>
2018-10-17 04:41:52 -0500

    3 to 1 Shifter
    Tested with testbench used to test 2 to 1 variant and passed except for ROR.

--- p1/Shifter_3_1.v
+++ p1/Shifter_3_1.v
@@ -13,57 +13,56 @@ wire[1:0] s_level0, s_level1, s_level2;
 wire[15:0] sll_out, sra_out, ror_out;
 
 // Create lookup table of base 3 select inputs
-assign s_level0 = Shift_In == 0 ? 0 :
-		  Shift_In == 1 ? 1 :
-		  Shift_In == 2 ? 2 :
-		  Shift_In == 3 ? 0 :
-		  Shift_In == 4 ? 1 :
-		  Shift_In == 5 ? 2 :
-		  Shift_In == 6 ? 0 :
-		  Shift_In == 7 ? 1 :
-		  Shift_In == 8 ? 2 :
-		  Shift_In == 9 ? 0 :
-		  Shift_In == 10 ? 1 :
-		  Shift_In == 11 ? 2 :
-		  Shift_In == 12 ? 0 :
-		  Shift_In == 13 ? 1 :
-		  Shift_In == 14 ? 2 :
-		  Shift_In == 15 ? 0 : 0;
+assign s_level0 = Shift_Val == 0 ? 0 :
+		  Shift_Val == 1 ? 1 :
+		  Shift_Val == 2 ? 2 :
+		  Shift_Val == 3 ? 0 :
+		  Shift_Val == 4 ? 1 :
+		  Shift_Val == 5 ? 2 :
+		  Shift_Val == 6 ? 0 :
+		  Shift_Val == 7 ? 1 :
+		  Shift_Val == 8 ? 2 :
+		  Shift_Val == 9 ? 0 :
+		  Shift_Val == 10 ? 1 :
+		  Shift_Val == 11 ? 2 :
+		  Shift_Val == 12 ? 0 :
+		  Shift_Val == 13 ? 1 :
+		  Shift_Val == 14 ? 2 :
+		  Shift_Val == 15 ? 0 : 0;
 
-assign s_level1 = Shift_In == 0 ? 0 :
-		  Shift_In == 1 ? 0 :
-		  Shift_In == 2 ? 0 :
-		  Shift_In == 3 ? 1 :
-		  Shift_In == 4 ? 1 :
-		  Shift_In == 5 ? 1 :
-		  Shift_In == 6 ? 2 :
-		  Shift_In == 7 ? 2 :
-		  Shift_In == 8 ? 2 :
-		  Shift_In == 9 ? 0 :
-		  Shift_In == 10 ? 0 :
-		  Shift_In == 11 ? 0 :
-		  Shift_In == 12 ? 1 :
-		  Shift_In == 13 ? 1 :
-		  Shift_In == 14 ? 1 :
-		  Shift_In == 15 ? 2 : 0;
+assign s_level1 = Shift_Val == 0 ? 0 :
+		  Shift_Val == 1 ? 0 :
+		  Shift_Val == 2 ? 0 :
+		  Shift_Val == 3 ? 1 :
+		  Shift_Val == 4 ? 1 :
+		  Shift_Val == 5 ? 1 :
+		  Shift_Val == 6 ? 2 :
+		  Shift_Val == 7 ? 2 :
+		  Shift_Val == 8 ? 2 :
+		  Shift_Val == 9 ? 0 :
+		  Shift_Val == 10 ? 0 :
+		  Shift_Val == 11 ? 0 :
+		  Shift_Val == 12 ? 1 :
+		  Shift_Val == 13 ? 1 :
+		  Shift_Val == 14 ? 1 :
+		  Shift_Val == 15 ? 2 : 0;
 
-assign s_level3 = Shift_In == 0 ? 0 :
-		  Shift_In == 1 ? 0 :
-		  Shift_In == 2 ? 0 :
-		  Shift_In == 3 ? 0 :
-		  Shift_In == 4 ? 0 :
-		  Shift_In == 5 ? 0 :
-		  Shift_In == 6 ? 0 :
-		  Shift_In == 7 ? 0 :
-		  Shift_In == 8 ? 0 :
-		  Shift_In == 9 ? 1 :
-		  Shift_In == 10 ? 1 :
-		  Shift_In == 11 ? 1 :
-		  Shift_In == 12 ? 1 :
-		  Shift_In == 13 ? 1 :
-		  Shift_In == 14 ? 1 :
-		  Shift_In == 15 ? 1 : 0;
-		
+assign s_level2 = Shift_Val == 0 ? 0 :
+		  Shift_Val == 1 ? 0 :
+		  Shift_Val == 2 ? 0 :
+		  Shift_Val == 3 ? 0 :
+		  Shift_Val == 4 ? 0 :
+		  Shift_Val == 5 ? 0 :
+		  Shift_Val == 6 ? 0 :
+		  Shift_Val == 7 ? 0 :
+		  Shift_Val == 8 ? 0 :
+		  Shift_Val == 9 ? 1 :
+		  Shift_Val == 10 ? 1 :
+		  Shift_Val == 11 ? 1 :
+		  Shift_Val == 12 ? 1 :
+		  Shift_Val == 13 ? 1 :
+		  Shift_Val == 14 ? 1 :
+		  Shift_Val == 15 ? 1 : 0;
 
 // SLL
 wire[15:0] sll_1, sll_2, sll_3, sll_6, sll_9, SLL_In_L1, SLL_In_L2;
@@ -95,5 +94,9 @@ assign ror_9 = {ROR_In_L1[8], ROR_In_L1[7], ROR_In_L1[6], ROR_In_L1[5], ROR_In_L
 mux_3_1_array_16 LEVEL_0_RO(ROR_In_L1 , Shift_In, ror_1, ror_2, s_level0);
 mux_3_1_array_16 LEVEL_1_RO(ROR_In_L2 , ROR_In_L1, ror_3, ror_6, s_level1);
 mux_3_1_array_16 LEVEL_2_RO(ror_out , ROR_In_L2, ror_9, 0, s_level2);
+
+assign Shift_Out = Opcode[1:0] == 0 ? sll_out :
+		   Opcode[1:0] == 1 ? sra_out :
+		   Opcode[1:0] == 2 ? ror_out : 0; 
 
 endmodule
