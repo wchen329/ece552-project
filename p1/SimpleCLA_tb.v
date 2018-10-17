20021b8e15c68ddfa16e62a44936986ed29181bf
Zhenghao Gu <zgu55@wisc.edu>
2018-10-17 02:44:09 -0500

    added CLA-based RED block

--- p1/SimpleCLA_tb.v
+++ p1/SimpleCLA_tb.v
@@ -16,3 +16,21 @@ module SimpleCLA_tb();
         $finish;
     end
 endmodule
+
+module SimpleCLA_tb2();
+    reg[7:0] A, B, Ans;
+    wire[7:0] Sum;
+    reg[63:0] i;
+
+    CLAdder8E DUT(A, B, 1'b0, Sum,,,,);
+    initial begin
+        for (i=0; i <= 64'hFFFF; i=i+1) begin
+            {A, B} = $random;
+            #1;
+            Ans = A+B;
+            if (Sum != Ans) $display("Unexpected output: A:%d, B:%d, Ans:%d, Sum:%d", A, B, Ans, Sum);
+        end
+        $display("Test complete");
+        $finish;
+    end
+endmodule
--- p1/alu/RED.v
+++ p1/alu/RED.v
@@ -1,16 +1,20 @@
+module RED(A, B, Reduction);
+    input [15:0] A, B;
+    output [15:0] Reduction;
 
-module RED (A, B, Reduction); 
+    wire [11:0] sumA, sumB;
+    wire carryA, carryB;
+    wire [11:0] sumAB;
 
-input [15:0] A, B;
-output [15:0] Reduction; 
-wire [8:0] temp1, temp2;
-wire [9:0] temp3; 
+    CLAdder8E  adder0(.A(A[15:8]), .B(B[15:8]), .Cin(1'b0), .Sum(sumA[7:0]), .Cout(carryA), .overflow(), .zero(), .overflowDirection());
+    CLAdder8E  adder1(.A(A[7:0]), .B(B[7:0]), .Cin(1'b0), .Sum(sumB[7:0]), .Cout(carryB), .overflow(), .zero(), .overflowDirection());
 
-adder_8bit ADD1(.Sum(temp1), .A(A[15:8]), .B(B[15:8]));
-adder_8bit ADD2(.Sum(temp2), .A(A[7:0]), .B(B[7:0])); 
+    assign sumA[11:8] = ((A[15] == 1'b0) & (B[15] == 1'b0)) ? {3'b000, carryA} :
+                        ((A[15] == 1'b1) & (B[15] == 1'b1)) ? 4'b1111 : {4{~carryA}};
+    assign sumB[11:8] = ((A[7] == 1'b0) & (B[7] == 1'b0)) ? {3'b000, carryB} :
+                        ((A[7] == 1'b1) & (B[7] == 1'b1)) ? 4'b1111 : {4{~carryB}};
 
-adder_9bit ADD3(.Sum(temp3), .A(temp1), .B(temp2)); 
+    CLAdder12E adder2(.A(sumA), .B({sumB}), .Cin(1'b0), .Sum(sumAB), .Cout(), .overflow(), .zero(), .overflowDirection());
 
-assign Reduction = {{6{temp3[9]}}, temp3}; //SIGN EXTENDED 10-bit temp TO 16-BIT
-
-endmodule 
+    assign Reduction = { {4{sumAB[11]}}, sumAB };
+endmodule
