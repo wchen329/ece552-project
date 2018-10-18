10c782f78661668ba38073d11195106be48be8a5
Zhenghao Gu <zgu55@wisc.edu>
2018-10-17 22:54:56 -0500

    added testbench for CLAdder8E

--- p1/SimpleCLA_tb.v
+++ p1/SimpleCLA_tb.v
@@ -1,4 +1,4 @@
-module SimpleCLA_tb();
+module CLAdder16_tb();
     reg[15:0] A, B, Ans;
     wire[15:0] Sum;
 
@@ -17,18 +17,38 @@ module SimpleCLA_tb();
     end
 endmodule
 
-module SimpleCLA_tb2();
-    reg[7:0] A, B, Ans;
+module CLAdder8E_tb();
+    reg[7:0] A, B;
     wire[7:0] Sum;
-    reg[63:0] i;
+    wire Cout, overflow, zero, direction;
 
-    CLAdder8E DUT(A, B, 1'b0, Sum,,,,);
+    reg [7:0] Ans;
+
+    reg[15:0] A3, B3, Ans3;
+
+    reg signed[7:0] A2, B2;
+    reg signed[15:0] Ans2;
+    reg signed[31:0] i;
+
+    CLAdder8E DUT(A, B, 1'b0, Sum, Cout, overflow, zero, direction);
+
     initial begin
-        for (i=0; i <= 64'hFFFF; i=i+1) begin
-            {A, B} = $random;
+        for (i=0; i<32'h20000; i=i+1) begin
+            {A,B} = i[15:0];
+            {A2, B2} = {A,B};
+            A3 = A;
+            B3 = B;
             #1;
             Ans = A+B;
-            if (Sum != Ans) $display("Unexpected output: A:%d, B:%d, Ans:%d, Sum:%d", A, B, Ans, Sum);
+            Ans2 = A2+B2;
+            Ans3 = A3+B3;
+
+            if (Ans != Sum) $display("Sum error");
+            if (Cout != Ans3[8]) $display("Cout error");
+            if (overflow != ((Ans2 > 127) | (Ans2 < -128))) $display("overflow error");
+            if (zero != (Ans == 0)) $display("zero error");
+            if (overflow)
+                if (direction == B[7]) $display("direction error");
         end
         $display("Test complete");
         $finish;
