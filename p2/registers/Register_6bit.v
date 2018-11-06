a75cf027c5ac195b00b61256d46b8e919d47850c
Winor Chen <wchen329@wisc.edu>
2018-11-05 19:44:06 -0600

    Added back write back including zero register write back and added pipeline registers and smaller bit width register

---
+++ p2/registers/Register_6bit.v
@@ -0,0 +1,13 @@
+/* A 6-bit register.
+ *
+ * wchen329@wisc.edu
+ */
+module Register(input clk, input rst, input [15:0] D, input WriteReg, input ReadEnable1, input ReadEnable2, inout[5:0] Bitline1, inout [5:0] Bitline2);
+	BitCell CELL_0(clk, rst, D[0], WriteReg, ReadEnable1, ReadEnable2, Bitline1[0], Bitline2[0]);
+	BitCell CELL_1(clk, rst, D[1], WriteReg, ReadEnable1, ReadEnable2, Bitline1[1], Bitline2[1]);
+	BitCell CELL_2(clk, rst, D[2], WriteReg, ReadEnable1, ReadEnable2, Bitline1[2], Bitline2[2]);
+	BitCell CELL_3(clk, rst, D[3], WriteReg, ReadEnable1, ReadEnable2, Bitline1[3], Bitline2[3]);
+	BitCell CELL_4(clk, rst, D[4], WriteReg, ReadEnable1, ReadEnable2, Bitline1[4], Bitline2[4]);
+	BitCell CELL_5(clk, rst, D[5], WriteReg, ReadEnable1, ReadEnable2, Bitline1[5], Bitline2[5]);
+
+endmodule 
