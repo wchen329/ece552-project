71f3768bc1e9ce8eade4abfe52e9e89a7767fc6d
Winor Chen <wchen329@wisc.edu>
2018-11-05 21:07:30 -0600

    Added enforcement of good form

--- memgen/Memgen.java
+++ memgen/Memgen.java
@@ -16,25 +16,34 @@ class Memgen
 	{
 		if(args.length != 1)
 		{
-			System.out.println("Error. Incorrect amount of parameters specified.");
-			System.out.println("Usage: java Memgen [amount of addressable lines]");
+			System.err.println("Error. Incorrect amount of parameters specified.");
+			System.err.println("Usage: java Memgen [amount of addressable lines]");
 			return;
 		}
 
 		char[] values = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
-		System.out.println("@0");
+		try
+		{
+			int lines = Integer.parseInt(args[0]);	// Raise exception if needed
+			System.out.println("@0");
 
-		Random rand = new Random();
+			Random rand = new Random();
 
 	
-		for(int i = 0; i < Integer.parseInt(args[0]); i++)
+			for(int i = 0; i < lines; i++)
+			{
+				System.out.print(values[rand.nextInt(16)]);
+				System.out.print(values[rand.nextInt(16)]);
+				System.out.print(values[rand.nextInt(16)]);
+				System.out.print(values[rand.nextInt(16)]);
+				System.out.print("\n");
+			}
+		}
+
+		catch(NumberFormatException e)
 		{
-			System.out.print(values[rand.nextInt(16)]);
-			System.out.print(values[rand.nextInt(16)]);
-			System.out.print(values[rand.nextInt(16)]);
-			System.out.print(values[rand.nextInt(16)]);
-			System.out.print("\n");
+			System.err.println("Error. The specified paramter must be an integer value.");
 		}
 	}
 }
