fc89cce2fa485d43fd5e8465bb8235d6fa53f08a
Zhenghao Gu <zgu55@wisc.edu>
2018-11-10 17:56:33 -0600

    use active high reset, see #36

--- p2/registers/FlagRegister.v
+++ p2/registers/FlagRegister.v
@@ -1,13 +1,13 @@
-module FlagRegister(clk, rst_n, weZVN, flagZVN_in, flagZVN_out);
-    input clk, rst_n;       // posedge trigger, sync active low reset
+module FlagRegister(clk, rst, weZVN, flagZVN_in, flagZVN_out);
+    input clk, rst;       // posedge trigger, sync active high reset
     input[2:0] weZVN, flagZVN_in; // {Z,V,N} order may be changed, active high write enable, e.g. if we[2]==1, then Z will be set to flag_in[2];
     output[2:0] flagZVN_out;
 
     wire[2:0] flag_out; // flipflop output
 
-    dff flagZ(.q(flag_out[2]), .d(flagZVN_in[2]), .wen(weZVN[2]), .clk(clk), .rst(~rst_n));
-    dff flagV(.q(flag_out[1]), .d(flagZVN_in[1]), .wen(weZVN[1]), .clk(clk), .rst(~rst_n));
-    dff flagN(.q(flag_out[0]), .d(flagZVN_in[0]), .wen(weZVN[0]), .clk(clk), .rst(~rst_n));
+    dff flagZ(.q(flag_out[2]), .d(flagZVN_in[2]), .wen(weZVN[2]), .clk(clk), .rst(rst));
+    dff flagV(.q(flag_out[1]), .d(flagZVN_in[1]), .wen(weZVN[1]), .clk(clk), .rst(rst));
+    dff flagN(.q(flag_out[0]), .d(flagZVN_in[0]), .wen(weZVN[0]), .clk(clk), .rst(rst));
 
     // passthrough
     assign flagZVN_out = {
--- p2/registers/PCRegister.v
+++ p2/registers/PCRegister.v
@@ -1,22 +1,22 @@
-module PCRegister(clk, rst_n, we, P, Q);
-    input clk, rst_n, we; // posedge trigger, sync active low reset, active high write enable
+module PCRegister(clk, rst, we, P, Q);
+    input clk, rst, we; // posedge trigger, sync active high reset, active high write enable
     input[15:0] P; // new execution addr
     output[15:0] Q; // current execution addr;
 
-    dff bit0(.q(Q[0]), .d(P[0]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit1(.q(Q[1]), .d(P[1]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit2(.q(Q[2]), .d(P[2]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit3(.q(Q[3]), .d(P[3]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit4(.q(Q[4]), .d(P[4]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit5(.q(Q[5]), .d(P[5]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit6(.q(Q[6]), .d(P[6]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit7(.q(Q[7]), .d(P[7]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit8(.q(Q[8]), .d(P[8]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit9(.q(Q[9]), .d(P[9]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit10(.q(Q[10]), .d(P[10]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit11(.q(Q[11]), .d(P[11]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit12(.q(Q[12]), .d(P[12]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit13(.q(Q[13]), .d(P[13]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit14(.q(Q[14]), .d(P[14]), .wen(we), .clk(clk), .rst(~rst_n));
-    dff bit15(.q(Q[15]), .d(P[15]), .wen(we), .clk(clk), .rst(~rst_n));
+    dff bit0(.q(Q[0]), .d(P[0]), .wen(we), .clk(clk), .rst(rst));
+    dff bit1(.q(Q[1]), .d(P[1]), .wen(we), .clk(clk), .rst(rst));
+    dff bit2(.q(Q[2]), .d(P[2]), .wen(we), .clk(clk), .rst(rst));
+    dff bit3(.q(Q[3]), .d(P[3]), .wen(we), .clk(clk), .rst(rst));
+    dff bit4(.q(Q[4]), .d(P[4]), .wen(we), .clk(clk), .rst(rst));
+    dff bit5(.q(Q[5]), .d(P[5]), .wen(we), .clk(clk), .rst(rst));
+    dff bit6(.q(Q[6]), .d(P[6]), .wen(we), .clk(clk), .rst(rst));
+    dff bit7(.q(Q[7]), .d(P[7]), .wen(we), .clk(clk), .rst(rst));
+    dff bit8(.q(Q[8]), .d(P[8]), .wen(we), .clk(clk), .rst(rst));
+    dff bit9(.q(Q[9]), .d(P[9]), .wen(we), .clk(clk), .rst(rst));
+    dff bit10(.q(Q[10]), .d(P[10]), .wen(we), .clk(clk), .rst(rst));
+    dff bit11(.q(Q[11]), .d(P[11]), .wen(we), .clk(clk), .rst(rst));
+    dff bit12(.q(Q[12]), .d(P[12]), .wen(we), .clk(clk), .rst(rst));
+    dff bit13(.q(Q[13]), .d(P[13]), .wen(we), .clk(clk), .rst(rst));
+    dff bit14(.q(Q[14]), .d(P[14]), .wen(we), .clk(clk), .rst(rst));
+    dff bit15(.q(Q[15]), .d(P[15]), .wen(we), .clk(clk), .rst(rst));
 endmodule
