8c8854354d7c84194b56416f923ce11787e0ddfc
wchen329 <wchen329@wisc.edu>
2018-11-11 12:43:07 -0600

    Reset signal adjustment

--- p2/registers/IFID_Pipe_Register.v
+++ p2/registers/IFID_Pipe_Register.v
@@ -1,10 +1,10 @@
 /* The pipeline register specifically for IFID
  *
- * The reset signal is active low.
+ * The reset signal is active high.
  *
  * wchen329@wisc.edu
  */
-module IFID_Pipe_Register(input clk, input rst_n, input [15:0] inst_next, output[15:0] inst);
+module IFID_Pipe_Register(input clk, input rst, input [15:0] inst_next, output[15:0] inst);
 
-	Register IFID(.clk(clk), .rst(~rst_n), .D(inst_next), .WriteReg(1), .ReadEnable1(1), .ReadEnable2(0), .Bitline1(inst));
+	Register IFID(.clk(clk), .rst(rst), .D(inst_next), .WriteReg(1), .ReadEnable1(1), .ReadEnable2(0), .Bitline1(inst));
 endmodule
