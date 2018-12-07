07df35f366fcf7a6da7b1240a183bf807c57b581
wchen329 <wchen329@wisc.edu>
2018-12-06 23:47:39 -0600

    Added some more documentation and assigned a few more signals

--- p3/memory/Cache_Toplevel.v
+++ p3/memory/Cache_Toplevel.v
@@ -1,35 +1,47 @@
 /* A toplevel for a cache that connects to the cpu toplevel
  * The Cache Toplevel encapsulates a Cache Data Array and Tag Array 
- * and connects to the Main Memory task arbritrator in the event of a single
- * read port of the data memory directly in the event of two read ports.
- * Also includes fill FSM.
+ * and connects to the Main Memory task arbritrator / FSM in the event of a single
  *
+ * Addresses are 16 bits. Caches are 2 way associative, which results in 64
+ * sets. Each cache block is 16
+ * bytes.
+ *
+ * Addresses are thus partitioned as:
+ * [ 6 ]   [ 6 ]   [ 4 ]
+ * tag	   set	   byte offset
+ *
+ * Furthermore, each tag array block has 8 bit buses. The tag array block is
+ * partitioned as follows:
+ *
+ * [ 1 ]   [ 1 ]   [ 6 ]
+ * valid   lru     tag
  * -
+ *
+ * lru == 1 ? (least recently used) : (not most recently used);
  *
  * wchen329@wisc.edu
  */
-module Cache_Toplevel(clk, rst, Address_Oper, store, idle, Data_In, miss_state, Data_Out, cache_stall);
+module Cache_Toplevel(clk, rst, Address_Oper, store, r_enabled, cacheop, Data_In, Data_Out, miss_occurred);
 
 	input clk;			// Clock signal
 	input rst;			// reset signal
 	input [15:0] Address_Oper;	// address to perform memory operation
 	input [15:0] Data_In;		// input data to be used in case of a store
 	input store;			// true if store, false if load
-	input idle;			// true if operation is not memory operation, false if memory operation
+	input r_enabled;		// true if operation is memory operation, false if not memory operation
+	input [1:0] cacheop;		// encoded control signals for FSM to perform operations on caches. 0 = read, 1 = fill, 2 = set cache tag, 3 = X
 
-	output miss_state;		// cache is busy handling miss
-	output cache_stall;		// cache "busy filling" signal
+	output miss_occurred;
 	output [15:0] Data_Out;		// output data in case of load
 
-	wire cc_miss;			// current cycle miss
 	wire cc_valid;			// current cycle request is valid?
 	wire [127:0] block_decode;	// the block to get or store to
-	wire [15:0] cc_fill_address;	// current cycle address to write to MEMORY AND CACHE if wr. enabled
 	wire [7:0] word_select;		// the word within the block to retrieve
 	wire [7:0] tag_in;		// tag to update the cache with
 	wire [7:0] tag_out;		// tag that is read
 	wire cache_data_we;		// data write enable, passed from Fill FSM
 	wire cache_tag_we;		// tag write enable, passed from Fill FSM
+	wire [15:0] DataArray_Out;	// raw data leaving cache array
 
 	/* Evaluate cache miss signal
 	 * In order to evaluate the miss signal, "speculate" cache hit first,
@@ -39,13 +51,16 @@ module Cache_Toplevel(clk, rst, Address_Oper, store, idle, Data_In, miss_state, 
 	 */
 
 	// Assign wire signals
-	
+	assign miss_occurred = (~cc_valid & (tag_in != tag_out)); // a miss if the read tag is different from tag to write
+	assign Data_Out = {15{~miss_occurred}} & DataArray_Out; // a miss makes the data out NULL
+	assign cache_data_we = Cacheop == 2'b01 ? 1 : 0; 
+	assign cache_tag_we = Cacheop == 2'b10 ? 1 : 0;
+
+	// Create an admittedly giant decoder
+
 
 	// Cache Arrays
-	DataArray CACHE_DATA(.clk(clk), .rst(rst), .DataIn(Data_In), .Write(cache_data_we), .BlockEnable(block_decode), .WordEnable(word_select) .DataOut(Data_Out));
+	DataArray CACHE_DATA(.clk(clk), .rst(rst), .DataIn(Data_In), .Write(cache_data_we), .BlockEnable(block_decode), .WordEnable(word_select) .DataOut(DataArray_Out));
 	MetaDataArray TAG_ARRAY(.clk(clk), .rst(rst), .DataIn(tag_in), .Write(cache_tag_we), .BlockEnable(block_decode), .DataOut(tag_out));
-
-	// Cache Fill FSM
-	Cache_Fill_FSM FILL_FSM(.clk(clk), .rst_n(~rst), .miss_detected(cc_miss), .miss_address(Address_Oper). ,write_data_array(cache_data_we), .write_tag_array(cache_tag_we), .memory_address(cc_fill_address) ,  .memory_data_valid(cc_valid));
 
 endmodule
